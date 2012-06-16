4556cb24a8558db7e95e8da9f67db7e3f02c14bc
unknown <Eric Swann@Molly-Win.(none)>
2012-06-16 12:23:56 -0500

    Added in unit tests copied from Ninject service locator and adjusted for Autofac...all are running.

--- src/ServiceLocators/SimpleCqrs.Autofac/AutofacServiceLocator.cs
+++ src/ServiceLocators/SimpleCqrs.Autofac/AutofacServiceLocator.cs
@@ -7,7 +7,37 @@ namespace SimpleCqrs.Autofac
 {
     public class AutofacServiceLocator : IServiceLocator
     {
-       	private bool _isDisposing;
+
+        /// <summary>
+        /// Error message used when the type specified in the register methods is null
+        /// </summary>
+        public const string TypeNullErrorMessage = "The container does not accept null types to be registered";
+
+        /// <summary>
+        /// Error message used when the key specified in the register methods is null, empty or a string with white spaces only
+        /// </summary>
+        public const string KeyNullErrorMessage = "The key cannot be null, empty or a string with white spaces only";
+
+        /// <summary>
+        /// Error message used when the kernel is null
+        /// </summary>
+        public const string BuilderNullErrorMessage = "The specified builder cannot be null";
+
+        /// <summary>
+        /// Error message used when the reset method is called, actually it is not supported because Autofac does not suppor reseting the container
+        /// </summary>
+        public const string ResetingContainerErrorMessage = "Autofac does not support reseting the container";
+
+        /// <summary>
+        /// Error message used when the instance passed to the register methods is null
+        /// </summary>
+        public const string InstanceNullErrorMessage = "Null objects cannot be registered in the container";
+
+        /// <summary>
+        /// Error message used when the calling delegate passed to the register methods is null
+        /// </summary>
+        public const string CallingDelegateNullErrorMessage = "The calling delegate cannot be null";
+
         private ContainerBuilder _builder;
 
         private bool _registrationsUpdated;
@@ -16,13 +46,14 @@ namespace SimpleCqrs.Autofac
 
 		public AutofacServiceLocator(){}
 
-        public AutofacServiceLocator(IContainer container) : this()
-        {
-            _container = container;
-        }
 
         public AutofacServiceLocator(ContainerBuilder builder) : this()
         {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder", BuilderNullErrorMessage);
+            }
+
             _builder = builder;
         }
 
@@ -87,6 +118,11 @@ namespace SimpleCqrs.Autofac
 
 		public void Register<TInterface>(Type implType) where TInterface : class
 		{
+            if (implType == null)
+            {
+                throw new ArgumentNullException("implType", TypeNullErrorMessage);
+            }
+
 			var key = string.Format("{0}-{1}", typeof(TInterface).Name, implType.FullName);
 
 		    Builder.RegisterType(implType).Named<TInterface>(key);
@@ -103,27 +139,63 @@ namespace SimpleCqrs.Autofac
 
 		public void Register<TInterface, TImplementation>(string key) where TImplementation : class, TInterface
 		{
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException("key", KeyNullErrorMessage);
+            }
+
             Builder.RegisterType<TImplementation>().Named<TInterface>(key);
 		}
 
-		public void Register(string key, Type type)
+        public void Register(string key, Type implType)
 		{
-            Builder.RegisterType(type).Named(key, type);
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException("key", KeyNullErrorMessage);
+            }
+
+            if (implType == null)
+            {
+                throw new ArgumentNullException("implType", TypeNullErrorMessage);
+            }
+
+            Builder.RegisterType(implType).Named(key, implType);
 		}
 
 		public void Register(Type serviceType, Type implType)
 		{
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType", TypeNullErrorMessage);
+            }
+
+            if (implType == null)
+            {
+                throw new ArgumentNullException("implType", TypeNullErrorMessage);
+            }
+
             Builder.RegisterType(implType).As(serviceType);
 		}
 
 		public void Register<TInterface>(TInterface instance) where TInterface : class
 		{
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance", InstanceNullErrorMessage);
+            }
+
             Builder.RegisterInstance(instance).As<TInterface>();
 		}
 
 		public void Register<TInterface>(Func<TInterface> factoryMethod) where TInterface : class
 		{
-            Builder.Register(x => factoryMethod).As<TInterface>();
+            if (factoryMethod == null)
+            {
+                throw new ArgumentNullException("factoryMethod", CallingDelegateNullErrorMessage);
+            }
+
+            Builder.Register(x => factoryMethod()).As<TInterface>();
 		}
 
 		public void Release(object instance)
@@ -133,13 +205,15 @@ namespace SimpleCqrs.Autofac
 
 		public void Reset()
 		{
-            Dispose();
+            throw new NotImplementedException(ResetingContainerErrorMessage);
 		}
 
 		public TService Inject<TService>(TService instance) where TService : class
 		{
-			if (instance == null)
-				return null;
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance", InstanceNullErrorMessage);
+            }
 
 		    Container.InjectProperties(instance);
 			
@@ -151,16 +225,35 @@ namespace SimpleCqrs.Autofac
 			//Not needed for Autofac it doesn't keep references beyond the life cycle that was configured.
 		}
 
-		public void Dispose()
-		{
-			if (_isDisposing) return;
+        public bool IsDisposed { get; private set; }
 
-            if (_container == null) return;
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
 
-			_isDisposing = true;
-			_container.Dispose();
-			_container = null;
-		}
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (IsDisposed) return;
+            
+            if (disposing)
+            {
+                if (_container != null)
+                {
+                    _container.Dispose();
+                    _container = null;
+                }
+            }
+            IsDisposed = true;
+        }
+
 
         protected ContainerBuilder Builder
         {
